50a1ea0734802f73a22171a0cc9ae575b451a1f4
Cheater121 <106826455+Cheater121@users.noreply.github.com>
2022-06-13 10:54:42 +0300

    Add tests for msg

---
+++ Tests.py
@@ -0,0 +1,36 @@
+from jsonschema import validate
+
+
+schema = {
+	"type": "object",
+	"minProperties": 4,
+	"maxProperties": 4,
+	"properties": {
+		"Username": {
+			"type": "string",
+			"minLength": 1
+			},
+		"Timestamp": {
+			"type": "string",
+			"format": "date-time"
+			},
+		"Messagetext": {
+			"type": "string",
+			"minLength": 1
+			},
+		"Recipient": {
+			"type": "string",
+			"minLength": 1
+			}
+		}, 
+	"required": ["Username", "Timestamp", "Messagetext", "Recipient"]
+	}
+
+
+def msg_validator(msg):
+	try:
+		validate(msg, schema)
+	except:
+		return False
+	else:
+		return True
